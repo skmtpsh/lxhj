6faa3d66aa1b9ee2243d9b4448fa8c5641a001b5
pangshuhai <pangshuhai@126.com>
2018-08-20 13:29:40 +0800

    git add

---
+++ webproxy/index.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use( async ( ctx ) => {
+  ctx.body = process.env.NODE_ENV
+})
+// console.log(process.env.NODE_ENV)
+app.listen(3000)
+console.log('[demo] start-quick is starting at port 3000')
